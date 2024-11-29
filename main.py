16e5deabe564fadfe12fa083cac0f7dd8500975c
yinyajiang <32933356+yinyajiang@users.noreply.github.com>
2024-11-29 15:34:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def hook(window, ua, element_names, cookie_names):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Cookie loader with customizable parameters')
-    parser.add_argument('url', required=True, help='Target URL')
+    parser.add_argument('url', help='Target URL')
     parser.add_argument('--title', default='',
                         help='Window title')
     parser.add_argument('--ua', default='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
