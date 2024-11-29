8f840698e80e8186d7e1feff3bc64ce223d6ea23
yinyajiang <32933356+yinyajiang@users.noreply.github.com>
2024-11-29 14:32:05 +0800

    up

--- build_pyinstaller.bat
+++ build_pyinstaller.bat
@@ -1,14 +1,14 @@
 REM Check if the virtual environment exists
 IF NOT EXIST "venv" (
     REM Create virtual environment
-    python -m venv venv
+    python -m venv pyinstaller_venv
 )
 
 REM Activate virtual environment
-CALL venv\Scripts\activate
+CALL pyinstaller_venv\Scripts\activate
 
 pip install -r requirements.txt
 pip install pyinstaller
 
 REM Build
-python pyinstaller_build.py %*
+python build_pyinstaller.py %*
