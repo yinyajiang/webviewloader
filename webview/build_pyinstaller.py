749c74225cfc622c159f542081268557987c1131
yinyajiang <32933356+yinyajiang@users.noreply.github.com>
2024-12-18 09:45:38 +0800

    Update build_pyinstaller.py

--- webview/build_pyinstaller.py
+++ webview/build_pyinstaller.py
@@ -2,8 +2,9 @@ import os
 import sys
 import argparse
 import requests
+import shutil
 from build_cert import get_cert
-
+import subprocess
 current_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(current_dir)
 iswin = sys.platform.startswith('win')
@@ -14,6 +15,7 @@ parser.add_argument('--onedir', action='store_true')
 parser.add_argument('--name', default='load_cookie')
 parser.add_argument('--must-cert', action='store_true')
 parser.add_argument('--icon', default='')
+parser.add_argument('--win-sign', default='')
 args = parser.parse_args()
 
 cert = get_cert() if args.must_cert else ""
@@ -25,6 +27,14 @@ if args.icon and args.icon.startswith('http'):
     with open(dest, 'wb') as f:
         f.write(response.content)
     args.icon = dest
+elif args.icon:
+    dest = os.path.join(current_dir, "icon.ico")
+    shutil.copy2(args.icon, dest) 
+    args.icon = dest
+    
+# 确保图标文件路径是相对于当前目录的
+if args.icon:
+    args.icon = os.path.abspath(args.icon)
 
 import PyInstaller.__main__
 
@@ -47,3 +57,11 @@ pyinstaller_args = [
 ] if args.icon else [])
 
 PyInstaller.__main__.run(pyinstaller_args)
+
+_tmp = os.path.join(current_dir, "dist", args.name)
+if not args.onedir and os.path.exists(_tmp):
+    shutil.rmtree(_tmp)
+
+if iswin and args.win_sign:
+    subprocess.run(args.win_sign, shell=True)
+
